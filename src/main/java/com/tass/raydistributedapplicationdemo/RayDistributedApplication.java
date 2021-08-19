db1a6d2df11e69f60c45adc273061045c60f22bc
tasszz2k <tasszz2k@gmail.com>
2021-08-19 16:36:41 +0700

    init project

---
+++ src/main/java/com/tass/raydistributedapplicationdemo/RayDistributedApplication.java
@@ -0,0 +1,13 @@
+package com.tass.raydistributedapplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayDistributedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RayDistributedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tass/raydistributedapplicationdemo/RayDistributedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tass.raydistributedapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayDistributedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
